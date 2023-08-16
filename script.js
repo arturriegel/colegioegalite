bc7866f5033370f0896aaa2b7469d07029216d50
arturriegel <134106239+arturriegel@users.noreply.github.com>
2023-08-16 12:10:06 -0300

    v5.5 JS UPDATE

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+var crrs1 = document.getElementById('crrs1')
+var crrs2 = document.getElementById('crrs2')
+var crrs3 = document.getElementById('crrs3')
+var crrs = document.getElementById('carrosel')
 
+
+/*
 // Obtendo referências para os elementos HTML
 var btnm = document.getElementById('medio'); // Botão para o ensino médio
 var btnf = document.getElementById('fundamental'); // Botão para o ensino fundamental
@@ -9,6 +15,7 @@ var txtEns = document.querySelector('#selecaoens p'); // Parágrafo dentro do el
 // Função para lidar com o clique no botão de ensino infantil
 function btnInf() {
     if (selEns.style.width = '54.7vw') { // Verificando a largura do elemento de seleção
+        txtEns.style.color = 'white'
         selEns.style.width = '8vw'; // Definindo a largura do elemento de seleção
         btni.style.width = '54.7vw'; // Definindo a largura do botão de ensino infantil
         btnm.style.width = '8vw'; // Redefinindo a largura do botão de ensino médio
@@ -26,10 +33,13 @@ function btnFund() {
     btnf.style.width = '54.7vw'; // Definindo a largura do botão de ensino fundamental
     btni.style.width = '8vw'; // Redefinindo a largura do botão de ensino infantil
     btnm.style.width = '8vw'; // Redefinindo a largura do botão de ensino médio
+    selEns.removeChild(txtEns)
 }
 
 // Função para lidar com o clique no botão de ensino médio
 function btnMed() {
+    txtEns.style.color = 'white'
+    txtEns.style.color = 'white'
     selEns.style.width = '8vw'; // Definindo a largura do elemento de seleção
     btnm.style.width = '54.7vw'; // Definindo a largura do botão de ensino médio
     btnf.style.width = '8vw'; // Redefinindo a largura do botão de ensino fundamental
@@ -38,10 +48,12 @@ function btnMed() {
 
 // Função para lidar com o clique no botão de reset
 function btnRst() {
-    if (selEns.style.width = '8vw') { // Verificando a largura do elemento de seleção (CUIDADO: uso incorreto do operador de atribuição)
+    if (selEns.style.width == '8vw') { // Verificando a largura do elemento de seleção
+
         selEns.style.width = '54.7vw'; // Definindo a largura do elemento de seleção
         btnm.style.width = '8vw'; // Redefinindo a largura do botão de ensino médio
         btnf.style.width = '8vw'; // Redefinindo a largura do botão de ensino fundamental
         btni.style.width = '8vw'; // Redefinindo a largura do botão de ensino infantil
     }
 }
+*/
